4784a2256736c097a750ecb61d1e43d032fa5389
李韧 <lr_5146@163.com>
2015-11-08 12:16:39 +0800

    Initial Commit

---
+++ BUKPhotoEditViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BUKPhotoEditViewController
+//
+//  Created by lazy on 15/11/8.
+//  Copyright © 2015年 lazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BUKPhotoEditViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BUKPhotoEditViewController
+//
+//  Created by lazy on 15/11/8.
+//  Copyright © 2015年 lazy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
