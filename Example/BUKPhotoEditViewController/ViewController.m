5e05b616b07828f91c6d713f96d6158a721fc7af
LazyClutch <lr_5146@163.com>
2016-01-25 09:53:39 +0800

    delete a large image

--- Example/BUKPhotoEditViewController/ViewController.m
+++ Example/BUKPhotoEditViewController/ViewController.m
@@ -20,7 +20,7 @@
     
     self.navigationItem.title = @"照片编辑器";
     
-    BUKPhotoEditViewController *photoEditViewController = [[BUKPhotoEditViewController alloc] initWithPhoto:[UIImage imageNamed:@"11.jpg"]];
+    BUKPhotoEditViewController *photoEditViewController = [[BUKPhotoEditViewController alloc] initWithPhoto:[UIImage imageNamed:@"2.png"]];
     [self.navigationController pushViewController:photoEditViewController animated:YES];
     // Do any additional setup after loading the view, typically from a nib.
 }
